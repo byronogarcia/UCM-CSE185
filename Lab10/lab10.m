aa7b5f0e1ac4b7ccfa38b6087320cb5742a6b37c
Byron Garcia <byrongarcia48@yahoo.com>
2020-05-20 11:18:42 -0700

    Adding lab 10-11

---
+++ Lab10/lab10.m
@@ -0,0 +1,76 @@
+% Input values
+% img_dir = 'Army/';
+img_dir = 'Backyard/';
+% img_dir = 'Basketball/';
+
+img1 = im2double(imread(fullfile(img_dir, 'frame10.png')));
+img2 = im2double(imread(fullfile(img_dir, 'frame11.png')));
+
+% parameters
+window_size = 45;
+k = 4;
+
+w = floor(window_size/2);
+shift = w + 10;
+
+
+% convert to gray-scale
+I1 = rgb2gray(img1);
+I2 = rgb2gray(img2);
+
+% gradients
+Ix_m = imfilter(I1, [1 -1; 1 -1], 'replicate');
+Iy_m = imfilter(I1, [1 1; -1 -1], 'replicate');
+
+% compute optical flow
+u = zeros(size(I1)); u_next = zeros(size(I1));
+v = zeros(size(I2)); v_next = zeros(size(I1));
+
+
+for t = 1:k
+    for i = 1 + shift : size(Ix_m, 1) - shift
+        for j = 1 + shift : size(Ix_m, 2) - shift
+
+            %% extract Ix, Iy, It from local window
+            Ix = Ix_m(i-w : i+w, j-w : j+w);
+            Iy = Iy_m(i-w : i+w, j-w : j+w);
+
+            i2 = floor(i + v(i, j));
+            j2 = floor(j + u(i, j));
+
+            It = I1(i -w : i + w, j -w : j + w) - I2(i2-w : i2+w, j2-w : j2+w);
+            
+            %% convert Ix, Iy, It to vectors
+            Ix = Ix(:);
+            Iy = Iy(:);
+            It = It(:);
+            
+            %% construct matrix A and vector b
+            A = [Ix, Iy];
+            b = -It;
+%             x = A \ b;
+            x = A \ b;
+            
+            %% solve A x = b
+            u_next(i, j) = x(1);
+            v_next(i, j) = x(2);
+
+        end
+    end
+    %% update flow
+    u = u + u_next;
+    v = v + v_next;
+    
+    
+end
+
+plot_flow(img2, u, v);
+h = gcf;
+% saveas(h, 'result1.png');
+saveas(h, 'result2.png');
+% saveas(h, 'result3.png');
+
+%% imwrite(h, 'result1.png');
+%% imwrite(h, 'result2.png');
+%% imwrite(h, 'result3.png');
+
